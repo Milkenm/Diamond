871009b9d3618e321ae66ff9965dc7188413b32e
Milkenm <miguel.campos.johnny@gmail.com>
2019-09-28 17:58:10 +0100

    Add TestCodeSpeed function

---
+++ DiamondGui/Debug/TestCodeSpeed.cs
@@ -0,0 +1,24 @@
+﻿#region Usings
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+#endregion Usings
+
+
+
+namespace DiamondGui
+{
+	internal static partial class Debug
+	{
+		internal static void TestCodeSpeed(Action Code)
+		{
+			Stopwatch _StopWatch = new Stopwatch();
+
+			_StopWatch.Start();
+			Code.Invoke();
+			_StopWatch.Stop();
+
+			MessageBox.Show("Took: " + _StopWatch.ElapsedMilliseconds + " milliseconds.");
+		}
+	}
+}
