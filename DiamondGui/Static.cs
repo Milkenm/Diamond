551e1eeca93d3f936971cb05e8ef4cd0e95edd11
Milkenm <miguel.campos.johnny@gmail.com>
2019-10-09 20:44:40 +0100

    Add comment lines

--- DiamondGui/Static.cs
+++ DiamondGui/Static.cs
@@ -11,13 +11,16 @@ namespace DiamondGui
 {
 	internal static class Static
     {
+		// Forms
 		internal static Main MainForm;
 		internal static Options OptionsForm;
 		internal static Statistics StatisticsForm;
 
+		// Discord
         internal static DiscordSocketClient Client;
 		internal static CommandService Command;
 
+		// Other Items
 		internal static Settings Settings = new Settings();
     }
 }
