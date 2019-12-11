9649ad764b0923ef076221750b5823e95c52f471
Milkenm <miguel.campos.johnny@gmail.com>
2019-12-11 10:23:15 +0000

    Random Number Generator command

---
+++ DiamondGui/Modules/Commands/Tools/Generate Random Number.cs
@@ -0,0 +1,20 @@
+﻿#region Using
+using System;
+using System.Threading.Tasks;
+
+using Discord.Commands;
+#endregion
+
+
+
+namespace DiamondGui
+{
+	public partial class CommandsModule : ModuleBase<SocketCommandContext>
+	{
+		[Command("random number"), Alias("rnum", "rand num", "rand number", "random num", "rn", "random", "rnd", "rand"), Summary("Generates a random number between a min number and max.")]
+		public async Task GenerateRandomNumber(int min, int max)
+		{
+			await ReplyAsync("Random number: **" + new Random().Next(min, max) + "**");
+		}
+	}
+}
