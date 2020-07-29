e955d44d51a2cd94a8ef231f5ecae2c2ae82084b
Milkenm <miguel.campos.johnny@gmail.com>
2020-07-29 20:24:11 +0100

    Finish main program

---
+++ DiamondWpfGui/GUI/Main.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Diamond.Core;
+using Diamond.WPF.SQLite;
+using Discord;
+using System;
+using System.Data;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DiamondWpfGui
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class Main : Window
+	{
+		private static DiamondCore diamondCore;
+		private static SQLiteDB database;
+
+		public Main()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			database = new SQLiteDB(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Milkenm\Diamond\DiamondDB.db");
+
+			string token = null;
+			try
+			{
+				DataTable dt = database.ExecuteSQL("SELECT Value FROM Configs WHERE Config = 'BotToken'");
+
+				token = dt.Rows[0][0].ToString();
+			}
+			catch (Exception ex)
+			{
+				listBox_log.Items.Add(ex.Message);
+			}
+
+			if (!string.IsNullOrEmpty(token))
+			{
+				diamondCore = new DiamondCore(token, LogSeverity.Info, "!", Assembly.GetEntryAssembly(), Client_Log);
+			}
+		}
+
+		private Task Client_Log(LogMessage arg)
+		{
+			Dispatcher.Invoke(new Action(() => listBox_log.Items.Add(arg.Message)));
+			return Task.CompletedTask;
+		}
+
+		private void Button_start_Click(object sender, RoutedEventArgs e)
+		{
+			if (!diamondCore.IsRunning)
+			{
+				diamondCore.Start();
+			}
+			else
+			{
+				diamondCore.Stop();
+			}
+		}
+	}
+}
---
+++ DiamondWpfGui/SQLite/SQLite.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.Data.SQLite;
+
+namespace Diamond.WPF.SQLite
+{
+	public class SQLiteDB
+	{
+		private readonly string DatabasePath;
+
+		public SQLiteDB(string databasePath)
+		{
+			DatabasePath = databasePath;
+		}
+
+		public DataTable ExecuteSQL(string sql)
+		{
+			DataTable dt = new DataTable();
+
+			using (SQLiteConnection sqliteConnection = new SQLiteConnection($"Data Source={DatabasePath}; Version=3;"))
+			{
+				using (SQLiteCommand cmd = sqliteConnection.CreateCommand())
+				{
+					cmd.CommandText = sql;
+					SQLiteDataAdapter da = new SQLiteDataAdapter(cmd.CommandText, sqliteConnection);
+
+					da.Fill(dt);
+				}
+			}
+
+			return dt;
+		}
+	}
+}
