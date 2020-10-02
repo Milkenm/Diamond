34e62f90039d77623abd926aa62da08347649173
Milkenm <miguel.campos.johnny@gmail.com>
2020-10-02 12:11:33 +0100

    Update command logging

--- Diamond Brainz/Events/Command Events/Command Executed.cs
+++ Diamond Brainz/Events/Command Events/Command Executed.cs
@@ -7,16 +7,16 @@ using System.Threading.Tasks;
 
 namespace Diamond.Brainz.Events
 {
-    public partial class CommandEvents
-    {
-        public async Task CommandExecuted(Optional<CommandInfo> command, ICommandContext context, IResult result)
-        {
-            if (!string.IsNullOrEmpty(result?.ErrorReason) && command.IsSpecified)
-            {
-                await context.Channel.SendMessageAsync(result.ErrorReason).ConfigureAwait(false);
-
-                await GlobalData.Brainz.TriggerLogEventAsync($"{context.User.Username} executed the command \"{command.Value.Name}\" on server \"{context.Guild.Name}\".").ConfigureAwait(false);
-            }
-        }
-    }
+	public partial class CommandEvents
+	{
+		public async Task CommandExecuted(Optional<CommandInfo> command, ICommandContext context, IResult result)
+		{
+			await GlobalData.Brainz.TriggerLogEventAsync($"{context.User.Username} executed the command \"{command.Value.Name}\" on server \"{context.Guild.Name}\".").ConfigureAwait(false);
+			if (!string.IsNullOrEmpty(result?.ErrorReason) && command.IsSpecified)
+			{
+				await GlobalData.Brainz.TriggerLogEventAsync($"Error on command \"{command.Value.Name}\" (executed by \"{context.User.Username}\" on guild \"{context.Guild.Name}\")").ConfigureAwait(false);
+				await context.Channel.SendMessageAsync(result.ErrorReason).ConfigureAwait(false);
+			}
+		}
+	}
 }
