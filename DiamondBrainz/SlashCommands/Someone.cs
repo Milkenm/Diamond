2e00e7ab2a4de56fd01d1134103591af3afc3e2a
Milkenm <miguel.campos.johnny@gmail.com>
2023-05-28 04:41:30 +0100

    Add "include-offline", "without-role" and "only-in-this-channel" parameters

--- DiamondBrainz/SlashCommands/Someone.cs
+++ DiamondBrainz/SlashCommands/Someone.cs
@@ -14,7 +14,7 @@ namespace Diamond.API.SlashCommands
 {
 	public class Someone : InteractionModuleBase<SocketInteractionContext>
 	{
-		private readonly List<string> _kaomojisList = new List<string>()
+		private static readonly List<string> _kaomojisList = new List<string>()
 		{
 			"(◕‿◕✿)",
 			@"¯\_(ツ)_/¯",
@@ -28,7 +28,10 @@ namespace Diamond.API.SlashCommands
 
 		[DSlashCommand("someone", "Selects a random user.")]
 		public async Task SomeoneCommandAsync(
-			[Summary("with-role", "Only selects users that contain a specific role.")] IRole role = null,
+			[Summary("with-role", "Only select users that have a specific role.")] IRole withRole = null,
+			[Summary("without-role", "Only select users that do not have a specific role.")] IRole withoutRole = null,
+			[Summary("only-in-this-channel", "Only selects users that can see this channel.")] bool onlyInThisChannel = false,
+			[Summary("include-offline", "Set to true to include offline users.")] bool includeOffline = false,
 			[Summary("include-bots", "Set to true to include bots.")] bool includeBots = false,
 			[Summary("include-self", "Set to true to include yourself.")] bool includeSelf = false,
 			[ShowEveryone] bool showEveryone = false
@@ -41,10 +44,18 @@ namespace Diamond.API.SlashCommands
 			await this.Context.Guild.DownloadUsersAsync();
 
 			// Filter users
-			IEnumerable<SocketGuildUser> validUsers = this.Context.Guild.Users;
-			if (role != null)
+			IEnumerable<IUser> validUsers = !onlyInThisChannel ? this.Context.Guild.Users : await this.Context.Channel.GetUsersAsync().FlattenAsync();
+			if (withRole != null)
 			{
-				validUsers = validUsers.Where(u => u.Roles.Contains(role));
+				validUsers = validUsers.Where(u => (u as SocketGuildUser).Roles.Contains(withRole));
+			}
+			if (withoutRole != null)
+			{
+				validUsers = validUsers.Where(u => !(u as SocketGuildUser).Roles.Contains(withoutRole));
+			}
+			if (!includeOffline)
+			{
+				validUsers = validUsers.Where(u => u.Status != UserStatus.Offline);
 			}
 			if (!includeBots)
 			{
@@ -59,17 +70,17 @@ namespace Diamond.API.SlashCommands
 			if (!validUsers.Any())
 			{
 				embed.Title = "No users found";
-				await embed.SendAsync();
+				_ = await embed.SendAsync();
 				return;
 			}
 
 			// Select a user based on a random index
 			int index = RandomGenerator.GetInstance().Random.Next(0, validUsers.Count());
-			SocketGuildUser selectedUser = validUsers.ElementAt(index);
+			SocketGuildUser selectedUser = validUsers.ElementAt(index) as SocketGuildUser;
 
 			// Select a random kaomoji
-			int kaomojiIndex = RandomGenerator.GetInstance().Random.Next(0, this._kaomojisList.Count);
-			string kaomoji = this._kaomojisList[kaomojiIndex];
+			int kaomojiIndex = RandomGenerator.GetInstance().Random.Next(0, _kaomojisList.Count);
+			string kaomoji = _kaomojisList[kaomojiIndex];
 
 			embed.Title = kaomoji;
 			embed.Description = selectedUser.Mention;
